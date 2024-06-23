de0c94f5a360d6d4b7991453b24e59dd5df80f17
Derek Atabayev <derekatabayev4@gmail.com>
2024-06-22 19:11:31 -0600

    git merge conflict message was appearing in code

--- app/page.js
+++ app/page.js
@@ -3,13 +3,9 @@ import React from 'react'
 import LandingPage from "./landing page/page";
 export default function Home() {
   return (
-<<<<<<< HEAD
-    <main className="flex min-h-screen flex-col items-center justify-between ">
-     <LandingPage />
-=======
-    <main className="flex min-h-screen flex-col items-center justify-between p-24">
-     
->>>>>>> 03f128321711e3d9c809a1fab2f05138e4bdfc7d
+    <main className="flex min-h-screen flex-col items-center">
+      <LandingPage />
+
     </main>
   );
 }
